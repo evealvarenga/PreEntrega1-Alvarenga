7aee888a56060d4f14c970ffd11070f7173e0e96
evealvarenga <124215838+evealvarenga@users.noreply.github.com>
2023-02-19 19:02:24 -0300

    Cambios en condicionales

--- main.js
+++ main.js
@@ -2,44 +2,96 @@
 //Funciones
 function cuadrado(){
     let a = Number(prompt("Ingrese la medida del lado del cuadrado: "))
-    resultado = a*a
-    alert("El resultado del área del cuadrado es de: " + resultado)
+    if (a < 0){
+        alert("El número ingresado no es correcto. Inténtelo nuevamente")
+    } else{
+        resultado = a*a
+        alert("El resultado del área del cuadrado es de: " + resultado)
+    }
 }
 function rectangulo(){
     let b = Number(prompt("Ingrese la base del rectangulo: "))
-    let h = Number(prompt("Ingrese la altura del rectangulo: "))
-    resultado = b*h
-    alert("El resultado del área del rectangulo es de: " + resultado)
+    if (b < 0){
+        alert("El número ingresado no es correcto. Inténtelo nuevamente")
+    } else {
+        let h = Number(prompt("Ingrese la altura del rectangulo: "))
+        if (h < 0){
+            alert("El número ingresado no es correcto. Inténtelo nuevamente")
+        }else{
+            resultado = b*h
+            alert("El resultado del área del rectangulo es de: " + resultado)
+        }
+    }
 }
 function triangul(){
     let b = Number(prompt("Ingrese la base del triángulo: "))
-    let h = Number(prompt("Ingrese la altura del triángulo: "))
-    resultado = (b*h)/2
-    alert("El resultado del área del triángulo es de: " + resultado)
+    if (b < 0){
+        alert("El número ingresado no es correcto. Inténtelo nuevamente")
+    } else {
+        let h = Number(prompt("Ingrese la altura del triángulo: "))
+        if (h < 0){
+            alert("El número ingresado no es correcto. Inténtelo nuevamente")
+        }else{
+            resultado = (b*h)/2
+            alert("El resultado del área del triángulo es de: " + resultado)
+        }
+    }
 }
 function parale (){
     let b = Number(prompt("Ingrese la base del paralelogramo: "))
-    let h = Number(prompt("Ingrese la altura del paralelogramo: "))
-    resultado = b*h
-    alert("El resultado del área del paralelogramo es de: " + resultado)
+    if (b < 0){
+        alert("El número ingresado no es correcto. Inténtelo nuevamente")
+    } else {
+        let h = Number(prompt("Ingrese la altura del paralelogramo: "))
+        if (h < 0){
+            alert("El número ingresado no es correcto. Inténtelo nuevamente")
+        }else{
+            resultado = b*h
+            alert("El resultado del área del paralelogramo es de: " + resultado)
+        }
+    }
 }
 function trape(){
     let bMayus = Number(prompt("Ingrese la base mayor del trapecio: "))
-    let bMinus = Number(prompt("Ingrese la base menor del trapecio: "))
-    let h = Number(prompt("Ingrese la altura del trapecio: "))
-    resultado = ((bMayus*bMinus)/2)*h
-    alert("El resultado del área del trapecio es de: " + resultado)
+    if (bMayus < 0){
+        alert("El número ingresado no es correcto. Inténtelo nuevamente")
+    } else {
+        let bMinus = Number(prompt("Ingrese la base menor del trapecio: "))
+        if (bMinus < 0){
+            alert("El número ingresado no es correcto. Inténtelo nuevamente")
+        }else{
+            let h = Number(prompt("Ingrese la altura del trapecio: "))
+            if (h < 0){
+                alert("El número ingresado no es correcto. Inténtelo nuevamente")
+            }else{
+                resultado = ((bMayus*bMinus)/2)*h
+                alert("El resultado del área del trapecio es de: " + resultado)
+            }
+        }
+    }
 }
 function circle(){
     let r = Number(prompt("Ingrese el radio del círculo: "))
-    resultado = pi*r*2
-    alert("El resultado del área del círculo es de: " + resultado)
+    if (r < 0){
+        alert("El número ingresado no es correcto. Inténtelo nuevamente")
+    } else{
+        resultado = pi*r*2
+        alert("El resultado del área del círculo es de: " + resultado)
+    }
 }
 function rombo(){
     let dMayus = Number(prompt("Ingrese la primera diagonal del rombo: "))
-    let dMinus = Number(prompt("Ingrese la segunda diagonal del rombo: "))
-    resultado = (dMayus*dMinus)/2
-    alert("El resultado del área del rombo es de: " + resultado)
+    if (dMayus < 0){
+        alert("El número ingresado no es correcto. Inténtelo nuevamente")
+    } else {
+        let dMinus = Number(prompt("Ingrese la segunda diagonal del rombo: "))
+        if (dMinus < 0){
+            alert("El número ingresado no es correcto. Inténtelo nuevamente")
+        } else {
+            resultado = (dMayus*dMinus)/2
+            alert("El resultado del área del rombo es de: " + resultado)
+        }
+    }
 }
 
 const pi = 3.1416
@@ -50,19 +102,20 @@ let figura = null
 alert("Calculadora de áreas de figuras geométricas")
 
 while(i!="ESC"){
-    figura = prompt("¿Qué figura desea calcular?\n1. Cuadrado.\n2. Rectángulo.\n3. Triángulo\n4. Paralelogramo.\n5. Trapecio.\n6. Círculo.\n7. Rombo.")
-    if(figura = "1"){ cuadrado()
-    } else if( figura = "2"){
+    figura = Number(prompt("¿Qué figura desea calcular?\n1. Cuadrado.\n2. Rectángulo.\n3. Triángulo\n4. Paralelogramo.\n5. Trapecio.\n6. Círculo.\n7. Rombo."))
+    if(figura == 1){ 
+        cuadrado()
+    } else if( figura == 2){
         rectangulo()
-    }else if( figura = "3"){
+    }else if( figura == 3){
         triangul()
-    }else if( figura = "4"){
+    }else if( figura == 4){
         parale()
-    }else if( figura = "5"){
+    }else if( figura == 5){
         trape()
-    }else if( figura = "6"){
+    }else if( figura == 6){
         circle()
-    }else if( figura = "7"){
+    }else if( figura == 7){
         rombo()
     }else{
         alert("El número ingresado no es correcto.")
